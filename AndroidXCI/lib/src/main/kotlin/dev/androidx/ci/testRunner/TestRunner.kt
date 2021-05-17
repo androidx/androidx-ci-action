c098de1398b775bcee812f7e4e2a48d22ac848ce
Yigit Boyar <yboyar@gmail.com>
2021-05-17 14:46:41 -0700

    Add a CLI module to be able to invoke from command line (#11)
    During development, we probably just want to use ./gradlew run --args and once
    it is more or less stable, we can consider creating a dist to avoid recompiling it.
    
    This setup supports both.

--- AndroidXCI/lib/src/main/kotlin/dev/androidx/ci/testRunner/TestRunner.kt
+++ AndroidXCI/lib/src/main/kotlin/dev/androidx/ci/testRunner/TestRunner.kt
@@ -16,6 +16,8 @@
 
 package dev.androidx.ci.testRunner
 
+import com.google.auth.oauth2.ServiceAccountCredentials
+import dev.androidx.ci.config.Config
 import dev.androidx.ci.datastore.DatastoreApi
 import dev.androidx.ci.firebase.FirebaseTestLabApi
 import dev.androidx.ci.gcloud.GoogleCloudApi
@@ -24,11 +26,13 @@ import dev.androidx.ci.github.dto.ArtifactsResponse
 import dev.androidx.ci.github.zipArchiveStream
 import dev.androidx.ci.testRunner.vo.TestResult
 import dev.androidx.ci.testRunner.vo.UploadedApk
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import org.apache.logging.log4j.kotlin.logger
+import java.io.File
 import java.util.concurrent.TimeUnit
 import java.util.zip.ZipEntry
 
@@ -57,6 +61,10 @@ class TestRunner(
      * An optional filter to pick which build artifacts should be downloaded.
      */
     private val githubArtifactFilter: ((ArtifactsResponse.Artifact) -> Boolean) = { true },
+    /**
+     * The directory where results will be saved locally
+     */
+    private val outputFolder: File? = null,
 ) {
     private val logger = logger()
     private val testMatrixStore = TestMatrixStore(
@@ -100,10 +108,12 @@ class TestRunner(
             TestResult.IncompleteRun(th.stackTraceToString())
         }
         logger.trace("done running tests, will upload result to gcloud")
+        val resultJson = result.toJson().toByteArray(Charsets.UTF_8)
         googleCloudApi.upload(
             "final-results/$runId/testResult.json",
-            result.toJson().toByteArray(Charsets.UTF_8)
+            resultJson
         )
+        outputFolder?.resolve("result.json")?.writeBytes(resultJson)
         return result
     }
 
@@ -127,5 +137,53 @@ class TestRunner(
             name = zipEntry.name,
             bytes = bytes
         )
+    }
+
+    companion object {
+        fun create(
+            runId: String,
+            githubToken: String,
+            googleCloudCredentials: String,
+            ioDispatcher: CoroutineDispatcher,
+            outputFolder: File?
+        ): TestRunner {
+            val credentials = ServiceAccountCredentials.fromStream(
+                googleCloudCredentials.byteInputStream(Charsets.UTF_8)
+            )
+            return TestRunner(
+                googleCloudApi = GoogleCloudApi.build(
+                    Config.GCloud(
+                        credentials = credentials,
+                        bucketName = "androidx-ftl-test-results",
+                        bucketPath = "github-ci-action"
+                    ),
+                    context = ioDispatcher
+                ),
+                githubApi = GithubApi.build(
+                    Config.Github(
+                        owner = "androidX",
+                        repo = "androidx",
+                        token = githubToken
+                    )
+                ),
+                firebaseTestLabApi = FirebaseTestLabApi.build(
+                    config = Config.FirebaseTestLab(
+                        credentials = credentials
+                    )
+                ),
+                firebaseProjectId = "androidx-dev-prod",
+                datastoreApi = DatastoreApi.build(
+                    Config.Datastore(
+                        credentials = credentials
+                    ),
+                    context = ioDispatcher
+                ),
+                githubArtifactFilter = {
+                    it.name.contains("artifacts_room")
+                },
+                outputFolder = outputFolder,
+                runId = runId
+            )
+        }
     }
 }
