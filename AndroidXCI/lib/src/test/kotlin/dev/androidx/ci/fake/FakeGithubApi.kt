b95f2d60b1a9741eab7322a1a9622480829923e1
Yigit Boyar <yboyar@gmail.com>
2021-05-21 14:10:25 -0700

    Add github APIs to push status updates
    This API is good enough to show test status in a PR

--- AndroidXCI/lib/src/test/kotlin/dev/androidx/ci/fake/FakeGithubApi.kt
+++ AndroidXCI/lib/src/test/kotlin/dev/androidx/ci/fake/FakeGithubApi.kt
@@ -18,21 +18,44 @@ package dev.androidx.ci.fake
 
 import dev.androidx.ci.github.GithubApi
 import dev.androidx.ci.github.dto.ArtifactsResponse
+import dev.androidx.ci.github.dto.CommitInfo
+import dev.androidx.ci.github.dto.RunInfo
 import okhttp3.ResponseBody
 import okhttp3.internal.http.RealResponseBody
 import okio.Buffer
+import java.util.UUID
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
 class FakeGithubApi : GithubApi {
+    private val runInfos = mutableMapOf<String, RunInfo>()
+    private val commitInfos = mutableMapOf<String, CommitInfo>()
     private val artifacts = mutableMapOf<String, ArtifactsResponse>()
     private val zipArchives = mutableMapOf<String, ResponseBody>()
+
+    private fun getOrCreateRunInfo(runId: String): RunInfo {
+        return runInfos.getOrPut(runId) {
+            val sha = UUID.randomUUID().toString()
+            commitInfos[sha] = CommitInfo(
+                sha = sha,
+                state = CommitInfo.State.PENDING,
+                statuses = emptyList()
+            )
+            RunInfo(
+                id = runId,
+                name = "test-run",
+                url = "https://test/run/$runId",
+                headSha = sha
+            )
+        }
+    }
 
     fun putArtifact(
         runId: String,
         response: ArtifactsResponse
     ) {
-        artifacts[runId] = response
+        val runInfo = getOrCreateRunInfo(runId)
+        artifacts[runInfo.id] = response
     }
 
     fun putArchive(
@@ -69,5 +92,33 @@ class FakeGithubApi : GithubApi {
 
     override suspend fun zipArchive(path: String): ResponseBody {
         return zipArchives[path] ?: throwNotFound<ResponseBody>()
+    }
+
+    override suspend fun runInfo(runId: String): RunInfo {
+        return runInfos[runId] ?: throwNotFound<RunInfo>()
+    }
+
+    override suspend fun commitStatus(ref: String): CommitInfo {
+        return commitInfos[ref] ?: throwNotFound<CommitInfo>()
+    }
+
+    override suspend fun updateCommitStatus(sha: String, update: CommitInfo.Update): CommitInfo.Status {
+        val commitInfo = commitInfos[sha] ?: throwNotFound<CommitInfo.Status>()
+        val newStatus = CommitInfo.Status(
+            url = "https://status/$sha",
+            id = UUID.randomUUID().toString(),
+            description = update.description,
+            state = update.state,
+            targetUrl = update.targetUrl,
+            context = update.targetUrl
+        )
+        val combinedStatuses = (listOf(newStatus) + commitInfo.statuses).distinctBy {
+            it.context
+        }
+        commitInfos[sha] = commitInfo.copy(
+            statuses = combinedStatuses,
+            state = combinedStatuses.minByOrNull { it.state }?.state ?: CommitInfo.State.PENDING
+        )
+        return newStatus
     }
 }
